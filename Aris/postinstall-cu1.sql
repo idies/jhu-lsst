e53283a9beb117c9c3f1119aac13689697f635a1
suzanne werner <capnsue@gmail.com>
2020-01-27 22:41:47 -0500

    more postinstall cu1

--- Aris/postinstall-cu1.sql
+++ Aris/postinstall-cu1.sql
@@ -24,6 +24,14 @@ WITH (
         USE_TYPE_DEFAULT = TRUE)
 );
 
+if not exists(select * from sys.external_file_formats where name = 'parquet_file')
+	CREATE EXTERNAL FILE FORMAT parquet_file
+	WITH (  
+    FORMAT_TYPE = PARQUET,  
+    DATA_COMPRESSION = 'org.apache.hadoop.io.compress.SnappyCodec'  
+	);  
+
+
 
 use LSST;
 create login spark_user with password = '!!foospark9999'
@@ -69,6 +77,16 @@ EXECUTE('Use LSST; CREATE USER spark_user; ALTER ROLE [db_datareader] ADD MEMBER
 EXECUTE('Use LSST; grant create  table to spark_user;grant alter any schema to spark_user;') AT DATA_SOURCE SqlDataPool;
 
 
+--========================================================
+
+EXECUTE( '  CREATE LOGIN admin  WITH PASSWORD = ''fooRiuzg54'' ;') AT  DATA_SOURCE SqlDataPool;
+EXECUTE ('ALTER SERVER ROLE [sysadmin] ADD MEMBER [admin];') at DATA_SOURCE SqlDataPool;
+
+EXECUTE('Use LSST; CREATE USER spark_user; ALTER ROLE [db_datareader] ADD MEMBER spark_user;  ALTER ROLE [db_datawriter] ADD MEMBER spark_user; alter role [db_ddladmin] add member spark_user;') AT DATA_SOURCE SqlDataPool;
+
+EXECUTE('Use LSST; grant create  table to spark_user;grant alter any schema to spark_user;') AT DATA_SOURCE SqlDataPool;
+
+
 execute ('sp_configure "show advanced options", 1; RECONFIGURE;') at data_source SqlDataPool
 execute ('sp_configure "max server memory", 65536; RECONFIGURE;') at data_source SqlDataPool
 
@@ -79,16 +97,24 @@ GO
 create schema storage;
 go
 create schema dp;
-
+go
 
 USE LSST
 GO
 
-if not exists(select * from sys.external_file_formats where name = 'parquet_file')
-	CREATE EXTERNAL FILE FORMAT parquet_file
-	WITH (  
-    FORMAT_TYPE = PARQUET,  
-    DATA_COMPRESSION = 'org.apache.hadoop.io.compress.SnappyCodec'  
-	);  
+alter database LSST add filegroup STRIPEFG
+
+alter database LSST add file
+
+( NAME = N'stripe01', FILENAME = N'/var/opt/mssql/data/stripe01.ndf' , SIZE = 5GB , MAXSIZE = UNLIMITED, FILEGROWTH = 500MB ),
+( NAME = N'stripe02', FILENAME = N'/var/opt/mssql/data2/stripe02.ndf' , SIZE = 5GB , MAXSIZE = UNLIMITED, FILEGROWTH = 500MB ),
+( NAME = N'stripe03', FILENAME = N'/var/opt/mssql/data3/stripe03.ndf' , SIZE = 5GB , MAXSIZE = UNLIMITED, FILEGROWTH = 500MB ),
+( NAME = N'stripe04', FILENAME = N'/var/opt/mssql/data4/stripe04.ndf' , SIZE = 5GB , MAXSIZE = UNLIMITED, FILEGROWTH = 500MB ),
+( NAME = N'stripe05', FILENAME = N'/var/opt/mssql/data5/stripe05.ndf' , SIZE = 5GB , MAXSIZE = UNLIMITED, FILEGROWTH = 500MB ),
+( NAME = N'stripe06', FILENAME = N'/var/opt/mssql/data6/stripe06.ndf' , SIZE = 5GB , MAXSIZE = UNLIMITED, FILEGROWTH = 500MB ),
+( NAME = N'stripe07', FILENAME = N'/var/opt/mssql/data7/stripe07.ndf' , SIZE = 5GB , MAXSIZE = UNLIMITED, FILEGROWTH = 500MB )
+to filegroup STRIPEFG
+go
+
 
 
