7f5880b816211de6253b22a2f685085f3f052b42
suzanne werner <capnsue@gmail.com>
2020-10-29 13:02:12 -0400

    conflicts i dont see any conflicts

--- Aris/kube/dracula/cu4/ad/security-patch-dracula.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-azdata bdc config replace -c custom/control.json -j "$.security.activeDirectory.ouDistinguishedName=OU\=filedb\,DC\=sdss\,DC\=pha\,DC\=jhu\,DC\=edu"
-azdata bdc config replace -c custom/control.json -j "$.security.activeDirectory.dnsIpAddresses=[\"172.23.38.117\",\"172.23.251.123\"]"
-azdata bdc config replace -c custom/control.json -j "$.security.activeDirectory.domainControllerFullyQualifiedDns=[\"sdssdc5.sdss.pha.jhu.edu\",\"sdssdc2.sdss.pha.jhu.edu\"]"
-azdata bdc config replace -c custom/control.json -j "$.security.activeDirectory.domainDnsName=sdss.pha.jhu.edu"
-azdata bdc config replace -c custom/control.json -j "$.security.activeDirectory.clusterAdmins=[\"filedb-sysadmins\"]"
-azdata bdc config replace -c custom/control.json -j "$.security.activeDirectory.clusterUsers=[\"filedb-dbas\"]"
-#Example for providing multiple clusterUser groups: [\"bdcusergroup1\",\"bdcusergroup2\"]
